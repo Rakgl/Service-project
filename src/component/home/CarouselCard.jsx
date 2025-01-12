ad5241f682a7baa93d228a2627ead378f00963a8
K-taiyi <korm.taiyi289@gmail.com>
2025-01-12 15:18:17 +0700

    install icon

--- src/component/home/CarouselCard.jsx
+++ src/component/home/CarouselCard.jsx
@@ -1,6 +1,15 @@
+import "./CarouselStyle.scss"
+
 const CarouselCard = () => {
   return (
-    <div>CarouselCard</div>
+    <>
+      <div className="Card">
+        <h1>Trusted By Over 500 Clients Arround The World </h1>
+        <div>
+          
+        </div>
+      </div>
+    </>
   )
 }
 
