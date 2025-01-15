f470735a079b8cc090a918fa13e08b0bb98d659b
Rak <kosal.linnorak@camsolution.biz>
2025-01-15 16:18:30 +0700

    new update

--- src/component/home/carouselCard/CarouselCard.jsx
+++ src/component/home/carouselCard/CarouselCard.jsx
@@ -1,10 +1,15 @@
 import './CarouselStyle.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'; 
 
 const CarouselCard = () => {
   return (
     <>
       <div className="Card">
-        <h1>Trusted By Over 500 Clients Arround The World </h1>
+        <h1>
+          Trusted By Over 500 Clients Around The World{' '}
+          <FontAwesomeIcon icon={faFacebook} />
+        </h1>
         <div></div>
       </div>
     </>
