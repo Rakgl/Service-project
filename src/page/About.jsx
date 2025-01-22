db538089cd60dd160a720655ecc04172e3b2b164
Rak <kosal.linnorak@camsolution.biz>
2025-01-22 11:56:37 +0700

    new update

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -2,6 +2,7 @@ const AboutPage = () => {
   return (
     <>
       <div>About</div>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptate doloremque velit facilis modi, praesentium non cumque sed, consequuntur placeat repudiandae exercitationem voluptatem, nesciunt ea consequatur qui dolorum corporis eveniet?</p>
     </>
   );
 };
