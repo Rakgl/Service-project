7d57163849437b6fbbf8a5b77347f1522f847b94
Rak <kosal.linnorak@camsolution.biz>
2025-01-27 13:10:57 +0700

    new update the code

--- scripts/generateQRCode.js
+++ scripts/generateQRCode.js
@@ -4,6 +4,5 @@ const DEPLOYED_URL = 'https://service-project-vmli.vercel.app/';
 
 // Generate QR Code
 qrcode.generate(DEPLOYED_URL, { small: true }, () => {
-  console.log("\nScan this QR code to access your deployed app:");
   console.log(DEPLOYED_URL);
 });
