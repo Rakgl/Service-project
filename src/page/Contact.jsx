ba94488de311cbed144b715915f14cef2a910d14
Heng <hengkov653@gmail.com>
2025-02-01 10:21:14 +0700

    a

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,18 +1,27 @@
 import Scroll from "../component/scroll-up/Scroll";
 import Branch from "../component/contact-us/branch/Branch";
+import Map from "../component/contact-us/map/Map";
+import Form from "../component/contact-us/contact-form/Form";
 import PropHeader from "../component/home/PropHeader/PropHeader";
 
 const ContactPage = () => {
   return (
     <>
-        <PropHeader
+    <div>
+      <PropHeader
         title="Contact Us"
         breadcrumbs={[
           { label: "Home", link: "/" },
           { label: "Contact Us" },
         ]}/>
       <Branch/>
+      <Map/>
+      <div className="mt-5">
+        <Form/>
+      </div>
       <Scroll />
+    </div>
+
     </>
   );
 };
