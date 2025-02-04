38dcb2a92ffa72473dd196936e43497057cae70d
K-taiyi <korm.taiyi289@gmail.com>
2025-02-04 10:35:23 +0700

    a

--- src/component/services/RightSide.jsx
+++ src/component/services/RightSide.jsx
@@ -4,59 +4,73 @@ import {
   faCode, faMagnifyingGlass, faChartLine, 
   faMobileAlt, faDatabase, faTable, 
   faArrowRightLong, faDownload,
-  // faFacebook
 } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook,faInstagram,faYoutube } from "@fortawesome/free-brands-svg-icons";
+
 
 const RightSide = () => {
   return (
     <div className="rightside">
-      <section className="right_item">
-        <h4>List of Services</h4>
-        <ul className="services-list" role="list" type="none">
-          {[
-            { icon: faCode, text: "Web Development" },
-            { icon: faChartLine, text: "Digital Marketing" },
-            { icon: faTable, text: "SaaS Products" },
-            { icon: faMobileAlt, text: "Apps Development" },
-            { icon: faMagnifyingGlass, text: "SEO Services" },
-            { icon: faDatabase, text: "Data Analysis" },
-          ].map((service, index) => (
-            <li key={index} className="right-list" role="listitem">
-              <FontAwesomeIcon icon={service.icon} title={service.text} /> {service.text} 
-              <FontAwesomeIcon icon={faArrowRightLong} />
+      <div className="right">
+        <section className="right_item">
+          <h4>List of Services</h4>
+          <ul className="services-list" role="list" type="none">
+            {[
+              { icon: faCode, text: "Web Development" },
+              { icon: faChartLine, text: "Digital Marketing" },
+              { icon: faTable, text: "SaaS Products" },
+              { icon: faMobileAlt, text: "Apps Development" },
+              { icon: faMagnifyingGlass, text: "SEO Services" },
+              { icon: faDatabase, text: "Data Analysis" },
+            ].map((service, index) => (
+              <li key={index} className="right-list" role="listitem">
+                <FontAwesomeIcon icon={service.icon} title={service.text} /> {service.text} 
+                <FontAwesomeIcon icon={faArrowRightLong} />
+              </li>
+            ))}
+          </ul>
+        </section>
+
+        <section className="right_item">
+          <h4>Download Assets</h4>
+          <p className="font-sizePx14">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore quibusdam maiores 
+            odit mollitia officiis et, dolor necessitatibus facere sit commodi sint illum.
+          </p>
+          <ul className="download-links" role="list" type="none">
+            <li className="second-link" role="listitem">
+              <a href="#service-report">
+                <FontAwesomeIcon icon={faDownload} /> Service Report <FontAwesomeIcon icon={faArrowRightLong} />
+              </a>
             </li>
-          ))}
-        </ul>
-      </section>
-
-      <section className="right_item">
-        <h4>Download Assets</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore quibusdam maiores 
-          odit mollitia officiis et, dolor necessitatibus facere sit commodi sint illum.
-        </p>
-        <ul className="download-links" role="list" type="none">
-          <li className="second-link" role="listitem">
-            <a href="#service-report">
-              <FontAwesomeIcon icon={faDownload} /> Service Report
-            </a>
-          </li>
-          <li className="second-link" role="listitem">
-            <a href="#all-services">
-              <FontAwesomeIcon icon={faDownload} /> All Services
-            </a>
-          </li>
-        </ul>
-
-        <ul className="social-links" type="none">
-          <p>Follow us</p>
-          <li>
-            <a href="" aria-label="Facebook">
-            {/* <FontAwesomeIcon icon={faFacebook} /> */}
-            </a>
-          </li>
-        </ul>
-      </section>
+            <li className="second-link" role="listitem">
+              <a href="#all-services">
+                <FontAwesomeIcon icon={faDownload} /> All Services <FontAwesomeIcon icon={faArrowRightLong} />
+              </a>
+            </li>
+          </ul>
+        </section>
+        <section className="rightitem">
+          <p className="font-md">Follow Us</p>
+          <ul className="social-links" type="none">
+            <li>
+              <a href="">
+              <FontAwesomeIcon icon={faFacebook} />
+              </a>
+            </li>
+            <li>
+              <a href="">
+              <FontAwesomeIcon icon={faInstagram} />
+              </a>
+            </li>
+            <li>
+              <a href="">
+              <FontAwesomeIcon icon={faYoutube} />
+              </a>
+            </li>
+          </ul>
+        </section>
+      </div>
     </div>
   );
 };
