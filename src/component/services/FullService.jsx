29bfc6a16e309b8be385d65dc3d11da244d8af9c
K-taiyi <korm.taiyi289@gmail.com>
2025-02-07 00:33:15 +0700

    a

--- src/component/services/FullService.jsx
+++ src/component/services/FullService.jsx
@@ -1,13 +1,22 @@
 import RightSide from "./RightSide"
 import LeftSide from "./LeftSide"
+import PropHeader from "../home/PropHeader/PropHeader"
 import "./serviceStyle.scss"
 
 const FullService = () => {
   return (
-    <main className="py-5 media">
-      <LeftSide/>
-      <RightSide/>
-    </main>
+    <div>
+      <PropHeader
+        title="Service Details"
+        breadcrumbs={[
+          { label: "Home", link: "/" },
+          { label: "Service Details" },
+        ]}/>
+      <div className="py-5 media">
+        <LeftSide/>
+        <RightSide/>
+      </div>
+    </div>
   )
 }
 
